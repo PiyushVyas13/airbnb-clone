8ed898072643d9cf157f55450f009685160af4b6
Piyush Vyas <vyaspiyush2208@gmail.com>
2023-08-15 19:47:51 +0530

    Initial commit from Create Next App

---
+++ app/(site)/page.tsx
@@ -0,0 +1,29 @@
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import getListings from "@/app/actions/getListings";
+import ListingCard from "@/app/components/listings/ListingCard";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if(listings.length === 0) {
+    return (<EmptyState showReset/>)
+  }
+  return (
+      <>
+        <Container>
+          <div className="pt-36 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+              {
+                  listings.map((listing: any) => {
+                      return (
+                          <ListingCard key={listing.id} data={listing} currentUser={currentUser}/>
+                      )
+                  })
+              }
+          </div>
+        </Container>
+      </>
+  )
+}
